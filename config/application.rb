c5bff07c1bbff254e3558e887f114a0e335f0f20
Mark Lu <marklu@marklu.net>
2011-09-30 16:11:27 -0700

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Shirtorder
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Compile assets
+    config.assets.compile = true
   end
 end
