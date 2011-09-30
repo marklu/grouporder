9754846d462f8d9d9118f7af0c444fd77081451f
Mark Lu <marklu@marklu.net>
2011-09-30 04:30:11 -0700

    Display event option totals

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,20 @@
 class Event < ActiveRecord::Base
   has_many :options
+  has_many :realms
+
+  def option_counts
+    realms = self.realms
+    options = Option.where(:event_id => self.id)
+    result = {}
+    options.each do |option|
+      result[option] = 0
+    end
+    realms.each do |realm|
+      optioncounts = realm.option_counts
+      optioncounts.each_key do |option|
+        result[option] += optioncounts[option]
+      end
+    end
+    return result
+  end
 end
