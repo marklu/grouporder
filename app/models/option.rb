a2aa9b7252bdcf85bf67d6d827ef2e9c3412ff21
Mark Lu <marklu@marklu.net>
2011-10-03 23:00:26 -0700

    Order limits per chapter

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,6 +1,16 @@
 class Option < ActiveRecord::Base
   belongs_to :event
+  
+  def at_limit_for(realm) 
+    if limit.nil?
+      return false
+    else
+      return realm.orders_with_option(self).size >= limit
+    end
+  end
+
   def to_s
     return name
   end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,13 +4,18 @@ class Order < ActiveRecord::Base
   has_many :payments
   validates :name, :length => { :minimum => 2 }
   validate :option_valid_for_event
+  validate :not_past_limit
 
   def option_valid_for_event
-    if option
-	  if option.event != realm.event
-        errors.add(:option, "can't be for another event")
-	  end
+	if option.event != realm.event
+      errors.add(:option, "can't be for another event")
 	end
+  end
+
+  def not_past_limit
+    if option.at_limit_for(realm)
+      errors.add(:option, "past limit for option")
+    end
   end
 
 end
